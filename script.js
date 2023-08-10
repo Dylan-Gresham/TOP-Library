9592fd90fa870f03c392053e788007dd7736dda1
Dylan-Gresham <dgresh724@gmail.com>
2023-08-09 17:42:12 -0700

    Begin creating js functions

---
+++ script.js
@@ -0,0 +1,14 @@
+let library = [];
+
+function Book(title, author, pages, read = false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(book) {
+    library.push(book);
+    
+    return;
+}
