d440d97004beee4f76b219eafbbfd01ed5a8a11d
Dylan-Gresham <dgresh724@gmail.com>
2023-08-14 14:40:33 -0600

    Implement adding form logic

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
+/* Library logic: */
 let library = [];
 
-function Book(title, author, pages, read = false) {
+// Create Book object constructor
+function Book(title, author, pages, read = false, rating = 'N/A') {
+    // Input validation
+    read = (read === "true") ? true : false;
+    
+    // Set values
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.rating = rating;
+
+    return;
 }
 
+// Get elements
 const newButton = document.getElementById('new');
 const deleteButton = document.getElementById('delete');
 const clearButton = document.getElementById('clear');
 const table = document.getElementById('table');
 const tableBody = document.getElementById('tbody');
+const dialogElement = document.getElementById('addDialog');
 
+// Logic for adding a book to both the library array and the table
 function addBookToLibrary(book) {
     library.push(book); // Add to array
 
@@ -53,11 +65,15 @@ function addBookToLibrary(book) {
     }
     newRead.appendChild(newCheckButton);
 
+    const newRating = document.createElement('td');
+    newRating.textContent = book.rating;
+
     // Add table cells to table row
     newRow.appendChild(newTitle);
     newRow.appendChild(newAuthor);
     newRow.appendChild(newPages);
     newRow.appendChild(newRead);
+    newRow.appendChild(newRating);
 
     // Add table row to table body
     tableBody.appendChild(newRow);
@@ -65,100 +81,9 @@ function addBookToLibrary(book) {
     return;
 }
 
-
+// Button Logic
 newButton.addEventListener('click', event => {
-    // Get title
-    let title = prompt("What's the title of the book?");
-    if(title === null) {
-        event.stopPropagation();
-
-        return;
-    } else {
-        title = title.trim();
-    }
-
-    while(title === undefined || title === "") {
-        title = prompt("What's the title of the book?");
-        if(title === null) {
-            event.stopPropagation();
-
-            return;
-        } else {
-            title = title.trim();
-        }
-    }
-
-    // Get author
-    let author = prompt(`Who's the author of ${title}?`);
-    if(author === null) {
-        event.stopPropagation();
-
-        return;
-    } else {
-        author = author.trim();
-    }
-
-    while(author === undefined || author === "") {
-        author = prompt(`Who's the author of ${title}?`);
-        if(author === null) {
-            event.stopPropagation();
-
-            return;
-        } else {
-            author = author.trim();
-        }
-    }
-
-    // Get number of pages in the book
-    let pages = prompt(`How many pages are in ${title}?`);
-    if(pages === null) {
-        event.stopPropagation();
-
-        return;
-    } else {
-        pages = pages.trim();
-    }
-
-    while(isNaN(pages) || pages === undefined || pages === "") {
-        pages = prompt(`How many pages are in ${title}?`);
-        if(pages === null) {
-            event.stopPropagation();
-
-            return;
-        } else {
-            pages = pages.trim();
-        }
-    }
-
-    // Get boolean for if user has finished the book (true) or not (false)
-    let completed = prompt(`Have you finished reading ${title}? Enter true or false:`).toLowerCase();
-    if(completed === null) {
-        event.stopPropagation();
-
-        return;
-    } else {
-        completed = completed.trim();
-    }
-
-    while(completed === undefined || completed === "" ||!(completed === 'true' || completed === 'false')) {
-        completed = prompt(`Have you finished reading ${title}? Enter true or false:`).toLowerCase();
-        if(completed === null) {
-            event.stopPropagation();
-
-            return;
-        } else {
-            completed = completed.trim();
-        }
-    }
-    if(completed === 'true') {
-        completed = true;
-    } else {
-        completed = false;
-    }
-
-    // Create the new book
-    let newBook = new Book(title, author, pages, completed);
-    addBookToLibrary(newBook); // Add to array & table
+    dialogElement.showModal();
 
     event.stopPropagation();
 });
@@ -212,3 +137,70 @@ deleteButton.addEventListener('click', event => {
 
     event.stopPropagation();
 });
+
+/* Adding Dialog Form Logic: */
+// Get elements of the dialog
+const dialogForm = document.getElementById('addDialogForm');
+const newTitle = document.getElementById('newTitle');
+const newAuthor = document.getElementById('newAuthor');
+const newPages = document.getElementById('newPages');
+const newFinished = document.getElementById('newFinished');
+const newRating = document.getElementById('newRating');
+const addButton = document.getElementById('addButton');
+const cancelButton = document.getElementById('cancelButton');
+
+
+// Cancel button triggers close event, book will be created and added here.
+dialogElement.addEventListener('close', event => {
+    // Handle if the dialog is canceled and not "submitted"
+    if(dialogElement.returnValue === "") {
+        event.stopPropagation();
+
+        return;
+    }
+
+    // Create array of return values
+    let returnTokens = dialogElement.returnValue.split(", ");
+    
+    // Create book and add to library/table
+    let newBook = new Book(returnTokens[0], returnTokens[1], +returnTokens[2], returnTokens[3], +returnTokens[4]);
+    addBookToLibrary(newBook);
+
+    event.stopPropagation();
+});
+
+// Implement explicit cancel button logic to override the required attributes
+cancelButton.addEventListener('click', event => {
+    dialogElement.close(); // No parameters with the close function so that the dialogElement listener catches it
+
+    event.stopPropagation();
+});
+
+// Validate then "Submit" the information entered.
+addButton.addEventListener('click', event => {
+    event.preventDefault(); // Stops submission to page itself
+    const numValidationRegex = /^[0-9]+$/;
+
+    let numPages = newPages.value;
+    let ratingNum = newRating.value;
+
+    if(!numPages.match(numValidationRegex)) {
+        event.stopPropagation();
+
+        return;
+    } else if(!ratingNum.match(numValidationRegex)) {
+        event.stopPropagation();
+
+        return;
+    }
+
+    // Close the dialog and "submit" the information
+    dialogElement.close(`${newTitle.value}, ${newAuthor.value}, ${numPages}, ${newFinished.checked}, ${ratingNum}`);
+
+    event.stopPropagation();
+});
+
+/* Deletion Dialog Form Logic: */
+// Get elements of the dialog
+
+// Cancel button triggers close event, book will be found and deleted here.
